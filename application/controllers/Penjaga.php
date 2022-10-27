8e691fef0b774d5a2ab6f00134e2412052330116
Hafiz Ambiya <hafizambiya5@gmail.com>
2022-10-27 19:25:48 +0700

    ganti di transaksi

--- application/controllers/Penjaga.php
+++ application/controllers/Penjaga.php
@@ -27,7 +27,6 @@ class Penjaga extends CI_Controller
 		$data['instansi'] = $this->M_crud->show('instansi', 'id_instansi DESC')->row();
 		$data['agenda'] = $this->M_crud->show('agenda', 'id_agenda DESC')->row();
 		$data['loket'] = $this->M_crud->get_id('loket', array('id_loket' => $id))->row();
-
 		$data['antrian'] = $this->M_crud->get_max_id('transaksi', 'no_antrian', array('id_loket' => $id, 'username' => $this->session->userdata('username'), 'tgl' => date('dmY')))->row();
 		$where = array('id_loket <' => 1, 'tgl' => date('dmY'));
 		$data['cek'] = $this->M_crud->get_id('transaksi', $where)->result();
@@ -40,7 +39,7 @@ class Penjaga extends CI_Controller
 	{
 		$tgl = date('dmY');
 		$where = array('id_loket <' => 1, 'tgl' => $tgl);
-		$antrian = $this->M_crud->get_id('transaksi', $where, 'no_antrian DESC');;
+		$antrian = $this->M_crud->get_id('transaksi', $where);
 		$data = array('no_antrian' => $antrian->row('no_antrian'), 'id_loket' => $this->session->userdata('loket'), 'username' => $this->session->userdata('username'), 'tgl' => $tgl);
 		//$this->M_crud->del('transaksi', array('no_antrian' => $antrian));
 		$w = array('id_transaksi' => $antrian->row('id_transaksi'));
